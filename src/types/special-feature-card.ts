9514c35e0f664647e0900d9e56e24d304b79b19a
이언덕 <110515401+leeunduck@users.noreply.github.com>
2025-10-29 03:36:15 +0900

    Feature/CDP-159-SelectModuleCard
    * CDP-159 refactor🔨 (card): card 컴포넌트 & special feature card 컴포넌트 리팩토링
    
    * CDP-159 feat✨ (card): select module card 추가

--- src/types/special-feature-card.ts
+++
@@ -1,8 +0,0 @@
-import type { HTMLAttributes } from "react";
-
-export interface SpecialFeatureCardProps extends Omit<HTMLAttributes<HTMLDivElement>, "title"> {
-  icon?: React.ReactNode;
-  title?: React.ReactNode;
-  description?: React.ReactNode;
-  children?: React.ReactNode;
-}
