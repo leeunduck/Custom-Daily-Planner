b17b3bd7724fd84b31a67e1ad1d79fee489a31a6
HyunseokLEE <unducklee@gmail.com>
2025-11-05 14:18:56 +0900

    CDP-171 feat✨ (landing): 히어로 cta 컴포넌트 추가

---
+++ src/components/landing/LandingHeroCtas.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/shared/button";
+import { Icon } from "@/shared/Icon";
+import Link from "next/link";
+
+export function LandingHeroCtas() {
+  return (
+    <div className={cn("flex flex-col gap-12 mt-20", "md:flex-row items-center gap-8 mt-10")}>
+      <Link href="/login">
+        <Button preset="hero" pill>
+          <span className="inline-flex items-center gap-4">
+            <Icon name="monitor" />
+            <span>Desktop</span>
+          </span>
+        </Button>
+      </Link>
+      <Link href="/login">
+        <Button preset="hero" pill>
+          <span className="inline-flex items-center gap-4">
+            <Icon name="laptop" />
+            <span>Mac</span>
+          </span>
+        </Button>
+      </Link>
+      <Link href="/login">
+        <Button preset="hero" pill>
+          <span className="inline-flex items-center gap-4">
+            <Icon name="smartphone" />
+            <span>iOS</span>
+          </span>
+        </Button>
+      </Link>
+    </div>
+  );
+}
