3253f8e704473d82b71342fa3bb3c5817d7a51c1
HyunseokLEE <unducklee@gmail.com>
2025-11-14 01:42:20 +0900

    CDP-204 feat✨ (login): form 애니메이션 추가

---
+++ src/types/motion.ts
@@ -0,0 +1,8 @@
+export type MotionDirection = "up" | "down" | "left" | "right";
+
+/** Tailwind 기반 페이드+슬라이드 옵션 */
+export interface FadeSlideOptions {
+  distanceClass?: string;
+
+  durationClass?: string;
+}
